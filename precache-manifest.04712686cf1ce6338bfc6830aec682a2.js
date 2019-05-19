eeba97a8dc3c9a6e2162473102676c25b3ce1366
Cortez <cortezguitar@gmail.com>
2019-05-19 21:37:27 +0300

    Updates

---
+++ precache-manifest.04712686cf1ce6338bfc6830aec682a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9ef047f58c5d43deba0d37af92cb275",
+    "url": "/TV-Schedule/index.html"
+  },
+  {
+    "revision": "cbd03fd0991b322c0c53",
+    "url": "/TV-Schedule/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "1a226c7572ef56614685",
+    "url": "/TV-Schedule/static/css/main.8ed02202.chunk.css"
+  },
+  {
+    "revision": "cbd03fd0991b322c0c53",
+    "url": "/TV-Schedule/static/js/2.06b3f2dd.chunk.js"
+  },
+  {
+    "revision": "1a226c7572ef56614685",
+    "url": "/TV-Schedule/static/js/main.b75e3592.chunk.js"
+  },
+  {
+    "revision": "5170140b24a4c1853922",
+    "url": "/TV-Schedule/static/js/runtime~main.9df59ded.js"
+  }
+]);
